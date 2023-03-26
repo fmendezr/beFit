dd12f47e6fce8072c8344ed7326f4e945634b8c9
Felix Mendez <mendez.felixrn@gmail.com>
2023-03-26 08:54:31 -0400

    create anvbar in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,34 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import { useDB } from "../contexts/DBContext";
+import friendsIcon from "../assets/friends.png";
+import beFit from "../assets/BeFit.png";
+import profile from "../assets/profile.png";
+import { useEffect, useState } from "react";
 
 export default function Home () {
 
     const { currentUser } = useAuth();
+    const { getUser } = useDB();
+    const [username, setUsername ] = useState("");
 
-    const displayName = "yes"
+    useEffect(() => {
+        const fetchData = async () => {
+            const rawUserData = await getUser(currentUser.uid)
+            if (rawUserData.exists()){
+                const userData = rawUserData.data();
+                setUsername(userData.username);
+            }
+        }
+        fetchData();
+    }, [])
 
-    return (<> 
-         <Link to={`/${displayName}`}><button>Profile</button></Link>
+    return (
+    <>
+        <div className="navbar">
+            <Link to="friends"><img className="mainIcon" src={friendsIcon}/></Link>
+            <img className="mainLogo" src={beFit}/>
+            <Link to={`/${username}`}><img className="mainIcon" src={profile}/></Link>
+        </div>    
     </>)
 }
